1f14feac369dccac92f56248c893eb8ffdfd5511
Shivani Shah <shivanishah96@hotmail.co.uk>
2018-01-10 21:36:22 +0000

    fixed rsa with signing, saving and loading files

---
+++ src/Adam.java
@@ -0,0 +1,100 @@
+
+import java.io.*;
+import java.security.*;
+import java.security.spec.*;
+ 
+public class Adam {
+ 
+	public static void main(String args[]) {
+		Adam adam = new Adam();
+		try {
+			String path = System.getProperty("user.dir");
+			System.out.println("Working Directory = " +
+					System.getProperty("user.dir"));
+ 
+			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA");
+ 
+			keyGen.initialize(1024);
+			KeyPair generatedKeyPair = keyGen.genKeyPair();
+ 
+			System.out.println("Generated Key Pair");
+
+
+
+			adam.dumpKeyPair(generatedKeyPair);
+			adam.SaveKeyPair(path, generatedKeyPair);
+ 
+			KeyPair loadedKeyPair = adam.LoadKeyPair(path, "DSA");
+			System.out.println("Loaded Key Pair");
+			adam.dumpKeyPair(loadedKeyPair);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+	}
+ 
+	private void dumpKeyPair(KeyPair keyPair) {
+		PublicKey pub = keyPair.getPublic();
+		System.out.println("Public Key: " + getHexString(pub.getEncoded()));
+ 
+		PrivateKey priv = keyPair.getPrivate();
+		System.out.println("Private Key: " + getHexString(priv.getEncoded()));
+	}
+ 
+	private String getHexString(byte[] b) {
+		String result = "";
+		for (int i = 0; i < b.length; i++) {
+			result += Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1);
+		}
+		return result;
+	}
+ 
+	public void SaveKeyPair(String path, KeyPair keyPair) throws IOException {
+		PrivateKey privateKey = keyPair.getPrivate();
+		PublicKey publicKey = keyPair.getPublic();
+ 
+		// Store Public Key.
+		X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(
+				publicKey.getEncoded());
+		FileOutputStream fos = new FileOutputStream(path + "/public.key");
+		fos.write(x509EncodedKeySpec.getEncoded());
+		fos.close();
+ 
+		// Store Private Key.
+		PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(
+				privateKey.getEncoded());
+		fos = new FileOutputStream(path + "/private.key");
+		fos.write(pkcs8EncodedKeySpec.getEncoded());
+		fos.close();
+	}
+ 
+	public KeyPair LoadKeyPair(String path, String algorithm)
+			throws IOException, NoSuchAlgorithmException,
+			InvalidKeySpecException {
+		// Read Public Key.
+		File filePublicKey = new File(path + "/public.key");
+		FileInputStream fis = new FileInputStream(path + "/public.key");
+		byte[] encodedPublicKey = new byte[(int) filePublicKey.length()];
+		fis.read(encodedPublicKey);
+		fis.close();
+ 
+		// Read Private Key.
+		File filePrivateKey = new File(path + "/private.key");
+		fis = new FileInputStream(path + "/private.key");
+		byte[] encodedPrivateKey = new byte[(int) filePrivateKey.length()];
+		fis.read(encodedPrivateKey);
+		fis.close();
+ 
+		// Generate KeyPair.
+		KeyFactory keyFactory = KeyFactory.getInstance(algorithm);
+		X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(
+				encodedPublicKey);
+		PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
+ 
+		PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(
+				encodedPrivateKey);
+		PrivateKey privateKey = keyFactory.generatePrivate(privateKeySpec);
+ 
+		return new KeyPair(publicKey, privateKey);
+	}
+}
--- src/Server.java
+++ src/Server.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.security.*;
+import javax.crypto.*;
 
 public class Server {
     private int portNumber = 15882;
@@ -9,7 +11,7 @@ public class Server {
     //Each client has a socketManager and the ArrayList adds a socketManager for the particular client on the list
     ArrayList<socketManager> clients = null;
 
-    public Server (int port) {
+    public Server(int port) {
         if (port > 2048) {
             portNumber = port;
         } else {
@@ -23,14 +25,15 @@ public class Server {
         System.out.println("Please enter 'Quit' to close the server");
         Scanner scan2 = new Scanner(System.in);
         String scanned = scan2.nextLine();
-        if(scanned=="QUIT") {
+        if (scanned == "QUIT") {
             sh.kill();
             System.out.println("Goodbye!");
         }
 
     }
+
     //Main Method:- called first when running the class file.
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Enter server port number: ");
         //User input of port number
         Scanner scan = new Scanner(System.in);
@@ -40,29 +43,28 @@ public class Server {
     }
 }
 
-class ServerHandler implements Runnable
-{
+class ServerHandler implements Runnable {
     int portNumber;
     ServerSocket serverSoc = null;
     ArrayList<socketManager> clients = null;
 
-    ServerHandler (int port) {
+    ServerHandler(int port) {
         portNumber = port;
     }
 
     public void run() {
-        try{
+        try {
             //Setup the socket for communication
             serverSoc = new ServerSocket(portNumber);
             clients = new ArrayList<socketManager>();
             //Infinite Loop
-            while (true){
+            while (true) {
 
                 //accept incoming communication
                 System.out.println("Waiting for client");
                 //listening for client
                 Socket soc = serverSoc.accept();
-                System.out.println("220 localhost:"+serverSoc.getLocalPort() + " Simple Mail Transfer Service Ready");
+                System.out.println("220 localhost:" + serverSoc.getLocalPort() + " Simple Mail Transfer Service Ready");
                 //Now creates new socketManager object
                 socketManager temp = new socketManager(soc);
                 //socketManager is added to the array list
@@ -74,32 +76,30 @@ class ServerHandler implements Runnable
                 schThread.start();
             }
 
-        }
-        catch (Exception except){
+        } catch (Exception except) {
             //Exception thrown (except) when something went wrong, pushing message to the console
             System.err.println("Error --> " + except.getMessage());
         }
 
 
     }
+
     //Method to shut down server
     public void kill() {
         System.out.println("Server shutting");
-        try{
+        try {
             serverSoc.close();
             for (socketManager client : clients) {
                 client.close();
             }
-        }
-        catch (Exception except){
+        } catch (Exception except) {
             //Exception thrown (except) when something went wrong, pushing message to the console
             System.err.println("Error --> " + except.getMessage());
         }
     }
 }
 
-class ServerConnetionHandler implements Runnable
-{
+class ServerConnetionHandler implements Runnable {
     socketManager selfs = null;
     ArrayList<socketManager> clients = null;
     boolean verbose = false;
@@ -107,84 +107,70 @@ class ServerConnetionHandler implements Runnable
     State previousState = State.NONE;
 
 
-    public ServerConnetionHandler (ArrayList<socketManager> l, socketManager inSoc){
+    public ServerConnetionHandler(ArrayList<socketManager> l, socketManager inSoc) {
         selfs = inSoc;
         clients = l;
     }
 
-    public ServerConnetionHandler (ArrayList<socketManager> l, socketManager inSoc, boolean v) {
+    public ServerConnetionHandler(ArrayList<socketManager> l, socketManager inSoc, boolean v) {
         selfs = inSoc;
         clients = l;
         verbose = v;
     }
 
-    public void run(){
-        try{
+    public void run() {
+        try {
             // Connection has been made with the client
-            selfs.output.writeUTF("220 localhost:"+selfs.ip() + ":" + selfs.port() + " Simple Mail Transfer Service Ready"  );
+            selfs.output.writeUTF("220 localhost:" + selfs.ip() + ":" + selfs.port() + " Simple Mail Transfer Service Ready");
             // Check if client wants to login or sign-up
             boolean check = false;
-            while(!check){
+            while (!check) {
                 String option = selfs.input.readUTF();
-                if (option.equals("signup")){
+                if (option.equals("signup")) {
                     selfs.output.writeUTF("Enter your name");
                     String name = selfs.input.readUTF();
                     selfs.output.writeUTF("Enter your username");
                     String username = selfs.input.readUTF();
                     selfs.output.writeUTF("Enter your password");
                     String pw = selfs.input.readUTF();
-                    AddUsers addUsers = new AddUsers(name,username,pw);
+                    AddUsers addUsers = new AddUsers(name, username, pw);
                     Boolean addCheck = addUsers.add();
-                    if (addCheck){
+                    if (addCheck) {
                         selfs.output.writeUTF("Success in signing up " + name);
+                    } else {
+                        selfs.output.writeUTF("There was an error adding you to the system. The username may be taken. Please try again");
                     }
-                    else{
-                        selfs.output.writeUTF("There was an error adding you to the system. The username may be taken");
-                    }
-                }else if (option.contains("login")){
+                } else if (option.contains("login")) {
                     System.out.println("login");
-                }else{
+                } else {
                     System.out.println("fail");
                 }
 
             }
 
 
-
-
-
-
-
-
             String heloCheck = selfs.input.readUTF();
-            if(heloCheck.contains("HELO")) {
+            if (heloCheck.contains("HELO")) {
                 boolean check2 = false;
-                while (!check2)
-                {	selfs.output.writeUTF("start");
+                while (!check2) {
+                    selfs.output.writeUTF("start");
                     String message = selfs.input.readUTF();
-                    String [] actionCheck = message.split(":");
+                    String[] actionCheck = message.split(":");
                     String action = actionCheck[0];
                     //SMTP commands using switch case
-
-
 
 
                 }
                 //close the stream once we are done with it
 
-            }else {
+            } else {
                 //this is hello error
             }
-        }
-        catch (Exception except){
+        } catch (Exception except) {
             //Exception thrown (except) when something went wrong, pushing message to the console
             System.out.println("Error in ServerHandler--> " + except.getMessage());
         }
     }
-
-
-
-
 
 
 
